061c16dec134b6f60ce1ce52825b7c53047ff59b
pinomartin <martin91pino@gmail.com>
2022-02-16 17:46:55 -0300

    UI changes on services page

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 import { Col } from "react-bootstrap";
 
-const ListItem = ({label, cols=6}) => {
+const ListItem = ({ label, cols = 6 }) => {
   return (
     <Col md={cols} className="mt-1 mb-1">
-      <div className={`${styles.services__box} d-flex align-items-center`}>
-        <i className={`${styles.services__icons} las la-check`}></i>
-        <h3 className={styles.services__box__title}>{label}</h3>
+      <div
+        className={`${styles.listItem__container} d-flex align-items-center`}
+      >
+        <i className={`${styles.listItem__icons} las la-check`}></i>
+        <h3 className={styles.listItem__label}>{label}</h3>
       </div>
     </Col>
   );
